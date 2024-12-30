0de18eabc83fa15e7d5b4faaf5692b9a60cdee53
Soham Sonar <soham.sonar427@gmail.com>
2024-12-29 23:45:55 -0600

    first commit

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,7 @@
+type Props = {}
+
+const LandingPageLayout = ({ children}: {children: React.ReactNode}) => {
+    return <div className="flex flex-col container relative">{children}</div>
+}
+
+export default LandingPageLayout
