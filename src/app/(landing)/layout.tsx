de038c437f440501a5b257883272c91925213716
Soham Sonar <soham.sonar427@gmail.com>
2024-12-31 23:58:11 -0600

    update LandingPage

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,7 +1,13 @@
 type Props = {}
 
 const LandingPageLayout = ({ children}: {children: React.ReactNode}) => {
-    return <div className="flex flex-col container relative">{children}</div>
+    return (
+        <div className="flex flex-col container relative">
+            <LandingPageNavbar></
+            LandingPageNavbar>
+            {children}
+        </div>
+    )
 }
 
 export default LandingPageLayout
